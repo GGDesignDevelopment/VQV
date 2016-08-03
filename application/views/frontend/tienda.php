7fbea927fe8a1e28ac7dd4808133f748bc639ef5
nachoGrosso <tbadell@gmail.com>
2016-08-03 02:05:07 -0300

    version 3 script carrito

--- application/views/frontend/tienda.php
+++ application/views/frontend/tienda.php
@@ -1,101 +1,104 @@
 <div class="contenedor">
-    <header>
-        <div class="titulo">
-            <h1>Tienda OnLine</h1>
-            <h2>Verde que te Quiero Verde</h2>
-        </div>
-        <nav>
-            <a href="#">&#xe0aa;</a>
-            <a href="#">&#xe0b1;</a>
-            <a href="#" class="boton" id="carrito_show"></a>
-        </nav>
-    </header>
-    <div class="contenido">
-        <nav id="filtro">
-            <a class="filter" href="#" data-categoria="0">Todas las categorias</a>
-            <?php if (count($categorias)): foreach ($categorias as $categoria): ?>
-                        <a class="filter" href="#" data-categoria="<?php echo $categoria->catid; ?>"><?php echo $categoria->catdescripcion; ?></a>
-                 <?php endforeach; ?>
-            <?php endif; ?> 
-        </nav>
-        <div class="productos">
-            <div id="col1" class="column"></div>
-            <div id="col2" class="column"></div>   
-            <div id="col3" class="column"></div>           
-        </div>
-    </div>
+		<header>
+				<div class="titulo">
+						<h1>Tienda OnLine</h1>
+						<h2>Verde que te Quiero Verde</h2>
+				</div>
+				<nav>
+						<a href="#">&#xe0aa;</a>
+						<a href="#">&#xe0b1;</a>
+						<a href="#" class="boton" id="login"></a>
+				</nav>
+		</header>
+		<div class="contenido">
+				<nav id="filtro">
+						<a class="filter" href="#" data-categoria="0">Todas las categorias</a>
+						<?php if (count($categorias)): foreach ($categorias as $categoria): ?>
+												<a class="filter" href="#" data-categoria="<?php echo $categoria->catid; ?>"><?php echo $categoria->catdescripcion; ?></a>
+								 <?php endforeach; ?>
+						<?php endif; ?> 
+				</nav>
+				<div class="productos">
+						<div id="col1" class="column"></div>
+						<div id="col2" class="column"></div>   
+						<div id="col3" class="column"></div>           
+				</div>
+		</div>
 </div>
-    <!-- Revisar pa' dinamico  -->
+		<!-- Revisar pa' dinamico  -->
 <div id="carrito">
-        <a href="index.html">vqv</a>
-        <form id="ingresar" method="POST" action="cart/login" name="iniciar">
-            <h2>Iniciar sesion</h2>
-            <input type="email" name="email" placeholder="email" required>
-            <input type="password" name="password" placeholder="contrase&ntilde;a"required>
-            <input type="submit" value="Ingresar">
-        </form>
-        <span></span>
-        <form id="registrarse" method="POST" action="cart/register" name="registrarse">
-            <h2>Registrarse</h2>
-            <input type="email" name="email" placeholder="email" required>
-            <input type="password" name="password" placeholder="contrase&ntilde;a" required>
-            <input type="password" name="password2" placeholder="confirmar contrase&ntilde;a" required>
-            <input type="text" name="name" placeholder="Nombre y Apellido" required>
-            <input type="text" name="phone" placeholder="Telefono">
-            <input type="text" name="address" placeholder="Direccion">
-            <input type="submit" value="Registrarse">
-        </form>
-        <form id="carritoForm" method="POST" action="#" style="display:none">
-            
-        </form>
-    </div>
-    <template id="prodTemplate">
-        <div class="prod">
-            <img src="img/{{prodimagen}}" title="{{prodnombre}}">
-            <a href="#" class="expandir" data-producto="{{prodid}}">{{prodnombre}}<span>&#x33;</span></a>
-            <form id="{{prodid}}" method="POST" action="cart/additem" class="oculto">
-                <div>
-                    <!-- {{#granel}}
-                        <select name="envase">
-                            <option selected disabled>Tipo de envase</option>
-                            <option value="1">Bolsa de papel</option>
-                            <option value="2">Envase de vidrio nuevo</option>
-                            <option value="3">Intercambio envase</option>
-                        </select>
-                    {{/granel}} -->
-                    <input type="number" name="cantidad" placeholder="cantidad">
-                    <span>{{produnidad}}</span>
-                </div>
-                <p>{{proddes}}</p>
-                <input type="hidden" name="id" value="{{prodid}}" >
-                <input type="submit" name="prodid" value="Agregar al" data-icon="&#xe015;">
-            </form>
-            
-        </div>
-    </template>
-    <template id="carritoTemplate">
-        <h2>{{name}}</h2>
-        <form method="POST" action="cart/confirm">
-            <input type="text" name="name" value="address" placeholder="{{adress}}">
-            {{#items}}
-            <fieldset>
-                <legend><a href="cart/removeItem/{{prodid}}" class="remove">&#xe019;</a></legend>
-                <p>{{prodnombre}}</p>
-                <div>
-                    <input type="number" name="cantidad" placeholder="{{quantity}}">
-                    <span>${{amount}}</span>
-                </div>
-            </fieldset>
-            {{/items}}
-        </form>
-    </template>
-    <template id="carritoItem">
-        <fieldset>
-            <legend><a href="cart/removeItem/{{prodid}}" class="remove">&#xe019;</a></legend>
-            <p>{{prodnombre}}</p>
-            <div>
-                <input type="number" name="cantidad" placeholder="{{quantity}}">
-                <span>${{amount}}</span>
-            </div>
-        </fieldset>
-    </template>
+	<a href="index.html">vqv</a>
+	<script id="loginForm" type="text/template">
+		<form id="ingresar" method="POST" action="#" name="iniciar">
+			<h2>Iniciar sesion</h2>
+			<input type="email" name="email" placeholder="email" required>
+			<input type="password" name="password" placeholder="contrase&ntilde;a"required>
+			<input type="submit" value="Ingresar">
+		</form>
+		<span></span>
+		<form id="registrarse" method="POST" action="cart/register" name="registrarse">
+			<h2>Registrarse</h2>
+			<input type="email" name="email" placeholder="email" required>
+			<input type="password" name="password" placeholder="contrase&ntilde;a" required>
+			<input type="password" name="password2" placeholder="confirmar contrase&ntilde;a" required>
+			<input type="text" name="name" placeholder="Nombre y Apellido" required>
+			<input type="text" name="phone" placeholder="Telefono">
+			<input type="text" name="address" placeholder="Direccion">
+			<input type="submit" value="Registrarse">
+		</form>
+	</script>
+	<script id="carritoTemplate" type="text/template">
+		<h2>{{name}}</h2>
+		<form method="POST" action="cart/confirm">
+			<input type="text" name="name" value="address" placeholder="{{adress}}">
+			{{#items}}
+			<fieldset>
+				<legend><a href="cart/removeItem/{{prodid}}" class="remove">&#xe019;</a></legend>
+				<p>{{prodnombre}}</p>
+				<div>
+						<input type="number" name="cantidad" placeholder="{{quantity}}">
+						<span>${{amount}}</span>
+				</div>
+			</fieldset>
+			{{/items}}
+		</form>
+		<a href="#" id="logout">Cerrar sesion</a>
+	</script>
+	<script type="text/template">
+		<fieldset>
+			<legend><a href="cart/removeItem/{{prodid}}" class="remove">&#xe019;</a></legend>
+			<p>{{prodnombre}}</p>
+			<div>
+				<input type="number" name="cantidad" placeholder="{{quantity}}">
+				<span>${{amount}}</span>
+			</div>
+		</fieldset>
+	</script>	
+</div>
+		<template id="prodTemplate">
+				<div class="prod">
+						<img src="img/{{prodimagen}}" title="{{prodnombre}}">
+						<a href="#" class="expandir" data-producto="{{prodid}}">{{prodnombre}}<span>&#x33;</span></a>
+						<form id="{{prodid}}" method="POST" action="cart/additem" class="oculto">
+								<div>
+										<!-- {{#granel}}
+												<select name="envase">
+														<option selected disabled>Tipo de envase</option>
+														<option value="1">Bolsa de papel</option>
+														<option value="2">Envase de vidrio nuevo</option>
+														<option value="3">Intercambio envase</option>
+												</select>
+										{{/granel}} -->
+										<input type="number" name="cantidad" placeholder="cantidad">
+										<span>{{produnidad}}</span>
+								</div>
+								<p>{{proddes}}</p>
+								<input type="hidden" name="id" value="{{prodid}}" >
+								<input type="submit" name="prodid" value="Agregar al" data-icon="&#xe015;">
+						</form>
+						
+				</div>
+		</template>
+		<template id="carritoItem">
+				
+		</template>
--- public_html/js/tienda.js
+++ public_html/js/tienda.js
@@ -1,140 +1,310 @@
-$(function () {
-    
-    // LOGIN
-    $('#ingresar').submit(function(e){
-        $.ajax({
-            type: 'POST',
-            url: $(this).attr('action'),
-            data: $(this).serialize(),
-            dataType: 'json',
-            success: function(json) {
-                carrito(json.msg);
-            }
-        });
-        return false;
-    });
+// $(function () {
+		
+// 		// LOGIN
+// 		$('#ingresar').submit(function(e){
+				// $.ajax({
+				// 		type: 'POST',
+				// 		url: $(this).attr('action'),
+				// 		data: $(this).serialize(),
+				// 		dataType: 'json',
+				// 		success: function(json) {
+				// 				carrito(json.msg);
+				// 		}
+				// });
+// 				return false;
+// 		});
 
-    // Dibujar carrito 
-    function dibujarCarrito() {
-        var carrTemplate = $('#carritoTemplate').html();
-        $.ajax({
-            type: 'GET',
-            url: 'cart/get',
-            dataType: 'json',
-            success: function(json){
-                $('#carritoForm').append(Mustache.render(carrTemplate, json))
-            }
-        })
-    }
+// 		// Dibujar carrito 
+// 		function dibujarCarrito() {
+// 				var carrTemplate = $('#carritoTemplate').html();
+// 				$.ajax({
+// 						type: 'GET',
+// 						url: 'cart/get',
+// 						dataType: 'json',
+// 						success: function(json){
+// 								$('#carritoForm').append(Mustache.render(carrTemplate, json))
+// 						}
+// 				})
+// 		}
 
-    // Mostrar Carrito 
-    function carrito(islog) {
-        if (islog == true) {
-            $('#ingresar').hide();
-            $('#registrarse').hide();
-            $('#carritoForm').show();
-            dibujarCarrito();
-        } else {
-            $('#ingresar').show();
-            $('#registrarse').show();
-            $('#carritoForm').hide();
-        }
-    }
+// 		// Mostrar Carrito 
+// 		function carrito(islog) {
+// 				if (islog == true) {
+// 						$('#ingresar').hide();
+// 						$('#registrarse').hide();
+// 						$('#carritoForm').show();
+// 						dibujarCarrito();
+// 				} else {
+// 						$('#ingresar').show();
+// 						$('#registrarse').show();
+// 						$('#carritoForm').hide();
+// 				}
+// 		}
 
-    // LogedIn
-    function isLogged() {
-        var $carrito = $('#carrito_show');
-        var islog = 'Mi Carrito <span>&#xe015;</span>';
-        var notlog = 'Iniciar / Registrarse';
-        $.ajax({
-            type: 'GET',
-            url: 'cart/islogged',
-            dataType: 'json',
-            success: function(json) {
-                if ( json.msg == true ) {
-                    $carrito.append(islog);
-                    carrito(true);
-                } else {
-                    $carrito.append(notlog);
-                    carrito(false);
-                }
-            }
-        })
-    };
+// 		// LogedIn
+// 		function isLogged() {
+// 				var $carrito = $('#carrito_show');
+// 				var islog = 'Mi Carrito <span>&#xe015;</span>';
+// 				var notlog = 'Iniciar / Registrarse';
+// 				$.ajax({
+// 						type: 'GET',
+// 						url: 'cart/islogged',
+// 						dataType: 'json',
+// 						success: function(json) {
+// 								if ( json.msg == true ) {
+// 										$carrito.append(islog);
+// 										carrito(true);
+// 								} else {
+// 										$carrito.append(notlog);
+// 										carrito(false);
+// 								}
+// 						}
+// 				})
+// 		};
 
-    isLogged();
+// 		isLogged();
 
-    // Register 
-    $('#registrarse').submit(function() {
-        $.ajax({
-            type: 'POST',
-            url: $(this).attr('action'),
-            data: $(this).serialize(),
-            dataType: 'json',
-            success: function(json) {
-                carrito(json.msg);
-            }
-        });
-        return false;
-    })
+// 		// Register 
+// 		$('#registrarse').submit(function() {
+				// $.ajax({
+				// 		type: 'POST',
+				// 		url: $(this).attr('action'),
+				// 		data: $(this).serialize(),
+				// 		dataType: 'json',
+				// 		success: function(json) {
+				// 				carrito(json.msg);
+				// 		}
+				// });
+// 				return false;
+// 		})
 
-    // Funcion para expandir el item producto.
-    $('.productos').delegate('.expandir', 'click', function(e) {
-        e.preventDefault();
-        var producto = $(this).attr('data-producto');
-        $('#'+producto).slideToggle();
-        $(this).toggleClass("fondo");
-    });
+// 		// Funcion para expandir el item producto.
+// 		$('.productos').delegate('.expandir', 'click', function(e) {
+// 				e.preventDefault();
+// 				var producto = $(this).attr('data-producto');
+// 				$('#'+producto).slideToggle();
+// 				$(this).toggleClass("fondo");
+// 		});
 
-    // Funcion para mostrar el carrito de compras
-    var $boton_carrito = $('#carrito_show');
-    var $contenedor = $('.contenedor');
-    var $carrito = $('#carrito');
-    
-
-    $boton_carrito.on('click', function() {
-        var estado = $carrito.css('display');
-        if ( estado == 'none') {
-            $contenedor.animate({width: '80%'}, function() {
-                $carrito.css('display', 'flex');
-            })
-        } else {
-            $carrito.css('display', 'none');
-            $contenedor.animate({width: '100%'});
-        }
-    });
+// Funcion para mostrar el carrito de compras
+var $boton_carrito = $('#login');
+var $contenedor = $('.contenedor');
+var $carrito = $('#carrito');
 
 
-
-    // Funcion dibujado y filtro de productos en AJAX
-    var producto = $('#prodTemplate').html();
-
-    function dibujar(categoria) {
-        $.ajax({
-            type: 'GET',
-            url: 'tienda/getProducts?catid='+categoria,
-            dataType: 'json',
-            success: function(json) {
-                var i = 1;
-                $.each(json, function(indice, obj) {
-                    $('#col'+i).append(Mustache.render(producto, obj));
-                    i = (i == 3 ? 1 : i + 1);
-                });
-            }
-        })  
-    };
-    
-    // inicializa con todos los productos
-    dibujar(0);
-    
-    var $boton = $('#filtro a');
-
-    $boton.on('click', function(e){
-        var categoria = $(this).attr('data-categoria');
-        $('#col1').empty();
-        $('#col2').empty();
-        $('#col3').empty();
-        dibujar(categoria);
-    });
-
+$boton_carrito.on('click', function() {
+		var estado = $carrito.css('display');
+		if ( estado == 'none') {
+				$contenedor.animate({width: '80%'}, function() {
+						$carrito.css('display', 'flex');
+				})
+		} else {
+				$carrito.css('display', 'none');
+				$contenedor.animate({width: '100%'});
+		}
 });
+
+
+
+// 		// Funcion dibujado y filtro de productos en AJAX
+// 		var producto = $('#prodTemplate').html();
+
+// 		function dibujar(categoria) {
+// 				$.ajax({
+// 						type: 'GET',
+// 						url: 'tienda/getProducts?catid='+categoria,
+// 						dataType: 'json',
+// 						success: function(json) {
+// 								var i = 1;
+// 								$.each(json, function(indice, obj) {
+// 										$('#col'+i).append(Mustache.render(producto, obj));
+// 										i = (i == 3 ? 1 : i + 1);
+// 								});
+// 						}
+// 				})  
+// 		};
+		
+// 		// inicializa con todos los productos
+// 		dibujar(0);
+		
+// 		var $boton = $('#filtro a');
+
+// 		$boton.on('click', function(e){
+// 				var categoria = $(this).attr('data-categoria');
+// 				$('#col1').empty();
+// 				$('#col2').empty();
+// 				$('#col3').empty();
+// 				dibujar(categoria);
+// 		});
+
+// });
+// var user = {
+// 	isloggedin: false,
+// 	init : function() {
+// 		this.cacheDOM();
+// 		this.bindEvents();
+// 		this.render();
+// 	},
+// 	cacheDOM : function() {
+// 		this.$boton = $('#login');
+// 		this.$carrito = $('#carrito');
+// 		this.$formLogin = this.$carrito.find('#ingresar');
+// 		this.$formRegister = this.$carrito.find('#registrarse'); 
+// 		this.islog = 'Mi Carrito <span>&#xe015;</span>';
+// 		this.notlog = 'Iniciar / Registrarse';
+// 		this.login = this.$carrito.find('#loginForm').html();
+// 		this.carritoTemplate = this.$carrito.find('#carritoTemplate').html(); 
+// 	},
+// 	bindEvents : function() {
+// 		this.$formLogin.submit(this.login);
+// 		this.$formRegister.submit(this.register);
+// 	},
+// 	render : function() {
+// 		if ( this.isLogged() == true ) {
+// 			this.$boton.html(this.islog);
+// 			$.ajax({
+// 					type: 'GET',
+// 					url: 'cart/get',
+// 					dataType: 'json',
+// 					success: function(json){
+// 							user.$carrito.html(Mustache.render(user.carritoTemplate, json));
+// 					}
+// 			})
+// 		} else {
+// 			this.$boton.html(this.notlog);
+// 			this.$carrito.html(this.login);
+// 		}
+// 	},
+// 	isLogged : function() {
+// 		var control = null;
+// 		$.ajax({
+// 			async: false,
+// 			type: 'GET',
+// 			url: 'cart/islogged',
+// 			dataType: 'json',
+// 			success: function(json) {
+// 				control = json.msg;
+// 			}
+// 		});
+// 		return control;
+// 	},
+// 	login : function() {
+		// $.ajax({
+		// 	type: 'POST',
+		// 	url: $(this).attr('action'),
+		// 	data: $(this).serialize(),
+		// 	dataType: 'json',
+		// 	success: function(json) {
+		// 		return false;
+		// 	}
+		// });
+		// return false;
+// 	},
+// 	logout : function() {
+
+// 	},
+// 	register : function() {
+		// $.ajax({
+		// 		type: 'POST',
+		// 		url: $(this).attr('action'),
+		// 		data: $(this).serialize(),
+		// 		dataType: 'json',
+		// 		success: function(json) {
+		// 				carrito(json.msg);
+		// 		}
+		// });
+		// return false;
+// 	}
+// };
+// user.init();
+var usuario = (function(){
+
+	// Cache DOM 
+	var $boton = $('#login');
+	var $carrito = $('#carrito');
+	// Contenido html boton principal
+	var islog = 'Mi Carrito <span>&#xe015;</span>';
+	var notlog = 'Iniciar / Registrarse';
+	// Templates MUSTACHE para render 
+	var loginValue = $carrito.find('#loginForm').html();
+	var carritoTemplate = $carrito.find('#carritoTemplate').html(); 
+
+	// Bind Events
+	$carrito.on('submit', '#ingresar', login);
+	$carrito.on('submit', '#registrarse', register);
+	$carrito.on('click', '#logout', logout);
+
+
+	function _render() {
+		if ( _isLogged() == true ) {
+			$boton.html(islog);
+			$.ajax({
+					type: 'GET',
+					url: 'cart/get',
+					dataType: 'json',
+					success: function(json){
+							$carrito.html(Mustache.render(carritoTemplate, json));
+					}
+			})
+		} else {
+			$boton.html(notlog);
+			$carrito.html(loginValue);
+		}
+	};
+
+	_render();
+
+	// Checkear si hay un usuario loggeado
+	function _isLogged() {
+		var control = null;
+		$.ajax({
+			async: false,
+			type: 'GET',
+			url: 'cart/islogged',
+			dataType: 'json',
+			success: function(json) {
+				control = json.msg;
+			}
+		});
+		return control;
+	};
+
+	function login(e){
+		$.ajax({
+			type: 'POST',
+			url: 'cart/login',
+			data: $(this).serialize(),
+			dataType: 'json',
+			success: _render(),
+		});
+		e.preventDefault();
+	};
+	
+
+	function register(e) {
+		alert('1');
+		$.ajax({
+				type: 'POST',
+				url: $(this).attr('action'),
+				data: $(this).serialize(),
+				dataType: 'json',
+				success: _render()
+		});
+		r.preventDefault();
+	};
+
+	function logout() {
+		$.ajax({
+			type: 'GET',
+			url: 'cart/logout',
+			success: _render()
+		})
+	}
+
+	function comprar() {
+		
+	}
+
+
+
+})();
