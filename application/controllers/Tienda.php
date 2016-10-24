ec51f80ee305efefc54ac499b16d7fcae9067063
jageitos <andresageitos@gmail.com>
2016-10-24 20:26:20 -0300

    getproducts remasterized
    .

--- application/controllers/Tienda.php
+++ application/controllers/Tienda.php
@@ -12,7 +12,7 @@ class Tienda extends Frontend_Controller {
         $this->data['title'] = 'VQV - Tienda on Line';
         $this->data['styles'][] = '<link rel="stylesheet" type="text/css" href="' . site_url('css/wip.css') . '">';
         $this->data['styles'][] = '<link rel="stylesheet" type="text/css" href="' . site_url('css/fonts.css') . '">';
-        $this->data['subview'] = 'frontend/wip';        
+        $this->data['subview'] = 'frontend/wip';
         $this->load->view('frontend/_layout_main', $this->data);
     }
 
@@ -24,8 +24,8 @@ class Tienda extends Frontend_Controller {
         $this->data['styles'][] = '<link rel="stylesheet" type="text/css" href="' . site_url('css/tienda.css') . '">';
         $this->data['scripts'][] = '<script src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>';
         $this->data['scripts'][] = '<script type="text/javascript" src="' . site_url('js/mustache.js') . '"></script>';
-        $this->data['scripts'][] = '<script type="text/javascript" src="' . site_url('js/tienda.js') . '"></script>'; 
-        
+        $this->data['scripts'][] = '<script type="text/javascript" src="' . site_url('js/tienda.js') . '"></script>';
+
         $this->data['categorias'] = $this->categoria_m->get();
 //        $productos = $this->producto_m->get();
 //        $i = 0;
@@ -39,11 +39,21 @@ class Tienda extends Frontend_Controller {
     }
 
     function getProducts() {
+        header('Access-Control-Allow-Origin: *'); 
         $catid = $this->input->get('catid');
-        if ($catid == NULL || $catid == 0) {
-            $products = $this->producto_m->get();            
+        $prodinicio = $this->input->get('inicio');
+        if ($this->input->get('granel') == 1) {
+          $prodgranel = 1;
+        }  else {
+          $prodgranel = "";
+        }
+
+        if ($prodinicio && $prodinicio == 1) {
+          $products = $this->producto_m->get(['prodinicio' => $prodinicio]);
+        } elseif ($catid == NULL || $catid == 0) {
+            $products = $this->producto_m->get(['prodgranel' => $prodgranel], FALSE);
         } else {
-            $products = $this->producto_m->get(['catid' => $catid], FALSE);
+            $products = $this->producto_m->get(['catid' => $catid, 'prodgranel' => $prodgranel], FALSE);
         }
         echo json_encode($products);
     }
